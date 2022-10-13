29c099163fb6d9dd9a19e0fbf70707fd6fa56563
Natalia <natalia.vayngolts@gmail.com>
2022-10-13 14:17:44 +0300

    feat: Add script with 3 initial entries for Postgres

---
+++ postgres/02_TestEntries.sql
@@ -0,0 +1,8 @@
+INSERT INTO template (language, framework, app_type, db_type, cloud_provider, iac_type, deployment_type, repo_url)
+VALUES ('python', 'fastapi', 'backend', 'postgres', 'aws', 'cdk', 'ecs', 'test_url_1');
+
+INSERT INTO template (language, framework, app_type, db_type, cloud_provider, iac_type, deployment_type, repo_url)
+VALUES ('python', 'flask', 'backend', 'mysql', 'azure', 'terraform', 'vm', 'test_url_2');
+
+INSERT INTO template (language, framework, app_type, db_type, cloud_provider, iac_type, deployment_type, repo_url)
+VALUES ('go', 'gin', 'backend', 'mongodb', 'aws', 'terraform', 'kubernetes', 'test_url_3');
