1d79d0f18aacebc411b8c9a54d851cbe47231460
Natalia <natalia.vayngolts@gmail.com>
2022-11-06 10:31:29 +0200

    chore: Change repo_urls for Postgres test entities

--- postgres/02_TestEntries.sql
+++ postgres/02_TestEntries.sql
@@ -1,8 +1,8 @@
 INSERT INTO template (language, framework, app_type, db_type, cloud_provider, iac_type, deployment_type, repo_url)
-VALUES ('python', 'fastapi', 'backend', 'postgres', 'aws', 'cdk', 'ecs', 'test_url_1');
+VALUES ('python', 'fastapi', 'backend', 'postgres', 'aws', 'cdk', 'ecs', 'otomato-gh/pull-test-ginroot');
 
 INSERT INTO template (language, framework, app_type, db_type, cloud_provider, iac_type, deployment_type, repo_url)
-VALUES ('python', 'flask', 'backend', 'mysql', 'azure', 'terraform', 'vm', 'test_url_2');
+VALUES ('python', 'flask', 'backend', 'mysql', 'azure', 'terraform', 'vm', 'otomato-gh/pull-test-ginroot');
 
 INSERT INTO template (language, framework, app_type, db_type, cloud_provider, iac_type, deployment_type, repo_url)
-VALUES ('go', 'gin', 'backend', 'mongodb', 'aws', 'terraform', 'kubernetes', 'test_url_3');
+VALUES ('go', 'gin', 'backend', 'mongodb', 'aws', 'terraform', 'kubernetes', 'otomato-gh/pull-test-ginroot');
